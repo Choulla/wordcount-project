5c825b85d7634fc7c042f48d1b455c37eb0f9f7e
choulla <mohammedweb4@gmail.com>
2020-03-18 21:23:06 +0100

    edited by CHOULLAMED

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.homepage,name="home"),
+	path('mmm',views.homepage,name="home"),
+		
 	path('counter/',views.count,name="count"),
 	path('about/',views.about,name="about"),
 ]
